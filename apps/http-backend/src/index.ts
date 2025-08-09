e1fbd3a70e3d9b176bbd79b0c36b6eb137f7c9ce
Sumeet Rana <sumeetsinh28@gmail.com>
2025-08-10 02:19:17 +0400

    feat: add zod types in common package

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -2,10 +2,23 @@ import express from "express";
 import jwt from "jsonwebtoken";
 import { JWT_SECRET } from "@repo/backend-common/config";
 import { auth } from "./middlewares/auth";
+import {
+  CreateUserSchema,
+  SignInSchema,
+  CreateRoomSchema,
+} from "@repo/common/types";
 
 const app = express();
 
 app.post("/signup", (req, res) => {
+  const data = CreateUserSchema.safeParse(req.body);
+
+  if (!data.success) {
+    return res.json({
+      message: "Incorrect inputs",
+    });
+  }
+
   // db call
   res.json({
     message: "Hello world",
@@ -13,6 +26,14 @@ app.post("/signup", (req, res) => {
 });
 
 app.post("/signin", (req, res) => {
+  const data = SignInSchema.safeParse(req.body);
+
+  if (!data.success) {
+    return res.json({
+      message: "Incorrect inputs",
+    });
+  }
+
   const userId = 1;
   const token = jwt.sign(
     {
@@ -25,6 +46,14 @@ app.post("/signin", (req, res) => {
 });
 
 app.post("/room", auth, (req, res) => {
+  const data = CreateRoomSchema.safeParse(req.body);
+
+  if (!data.success) {
+    return res.json({
+      message: "Incorrect inputs",
+    });
+  }
+
   // db call
   res.json({
     roomId: 123,
