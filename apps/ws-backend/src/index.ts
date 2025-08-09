351dd4ab5b02d21b0e72be68450b2838a502baae
Sumeet Rana <sumeetsinh28@gmail.com>
2025-08-10 01:42:53 +0400

    feat: sharing token in websocket and verifying tokens in websocket backend

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,8 +1,24 @@
 import { WebSocketServer } from "ws";
-
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { JWT_SECRET } from "./config";
+ 
 const wss = new WebSocketServer({ port: 8081 });
 
-wss.on("connection", (socket) => {
+wss.on("connection", (socket, request) => {
+  const url = request.url; // ws://localhost:8081?token=1234
+  if (!url) {
+    return;
+  }
+
+  const queryParams = new URLSearchParams(url.split("?")[1]);
+  const token = queryParams.get("token") || "";
+  const decoded = jwt.verify(token, JWT_SECRET);
+
+  if (!decoded || !(decoded as JwtPayload).userId) {
+    socket.close();
+    return;
+  }
+
   socket.on("message", (message) => {
     if (message.toString() == "ping") {
       socket.send("pong");
